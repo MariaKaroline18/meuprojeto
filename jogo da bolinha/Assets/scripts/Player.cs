464a3323416a7ce46f339367214a8974c6a4b37c
Maria Karoline Pereira da Silva <20221170150039@ifrn.local>
2023-10-11 10:15:31 -0300

    moverbolinha

--- jogo da bolinha/Assets/scripts/Player.cs
+++ jogo da bolinha/Assets/scripts/Player.cs
@@ -6,9 +6,13 @@ using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
+    
     public int velocidade = 10;
+    public int forcaPulo = 7;
 
     public Rigidbody rb;
+
+    public bool noChao = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +20,22 @@ public class Player : MonoBehaviour
 
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        noChao = true;
+    }
+
     // Update is called once per frame
     void Update()
     {
+        //pulo
+        if (noChao && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+            noChao = false;
+        }
+
+
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
         rb.AddForce(new Vector3(h, 0, v) * velocidade * Time.deltaTime, ForceMode.Impulse);
