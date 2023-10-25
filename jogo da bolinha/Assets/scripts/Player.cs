0e93eaa897349643fc1e0da38179dcb0174a1f19
Maria Karoline Pereira da Silva <20221170150039@ifrn.local>
2023-10-25 10:26:21 -0300

    Moedas

--- jogo da bolinha/Assets/scripts/Player.cs
+++ jogo da bolinha/Assets/scripts/Player.cs
@@ -16,32 +16,51 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("START");
         TryGetComponent(out rb);
 
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        noChao = true;
+        if (!noChao && collision.gameObject.tag == "Chão") 
+        {
+            noChao = true;
+        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("UPDAT");
         //pulo
-        if (noChao && Input.GetKeyDown(KeyCode.Space))
+        float h = Input.GetAxis("Horizontal");//-1 equerda ,0 nada ,1 direita
+        float v = Input.GetAxis("Vertical");//-1 pra trás ,0 nada ,1 pra frente
+        Vector3 direcao = new Vector3(h, 0, v);
+        rb.AddForce(direcao * velocidade * Time.deltaTime,ForceMode.Impulse);
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+            rb.AddForce(Vector3.up*forcaPulo,ForceMode.Impulse);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && noChao)
+        {
+            rb.AddForce(Vector3.up * forcaPulo,ForceMode.Impulse);
             noChao = false;
         }
 
-
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-        rb.AddForce(new Vector3(h, 0, v) * velocidade * Time.deltaTime, ForceMode.Impulse);
-        if (transform.position.y <= -10)
+        
+        if(transform.position.y <= -10)
         {
+            //jogador caiu
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            
+        
+        
+
+        
+           
         }
 
     }
